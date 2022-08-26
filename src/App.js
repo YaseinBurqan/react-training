976cbef6d168898290729fda77eef008057ebdf8
Yasein Burqan <mr.yasein@gmail.com>
2022-08-27 00:42:38 +0300

    create input FirstName and LastName With Focus after press Enter Using useRef

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import "./App.css";
-import React, { useContext } from "react";
-import Header from "./components/Header/Header";
-import LoginForm from "./components/LoginForm/LoginForm";
-import { AuthProvider, AuthContext } from "./components/Context/AuthContext/AuthContext";
+import React from "react";
+import Component from "./components/component/Component";
 
 function App() {
-  const authContext = useContext(AuthContext);
-
   return (
     <>
       <div className="App">
-        <Header />
-        {authContext.auth.email ? "welcome" : <LoginForm />}
+        <Component />
       </div>
     </>
   );
 }
 
-function AppWithAuth() {
-  return (
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  );
-}
-
-export default AppWithAuth;
+export default App;
